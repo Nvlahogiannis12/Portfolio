ad5d764aa76a4c88b3c57d5320fcc93c506b116d
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2026-05-01 12:32:31 +0000

    Planet made, (Retexture needed)

--- rendering.js
+++ rendering.js
@@ -1,24 +1,40 @@
+let angle = 0;
+let img;
+let LightCont = [255, 255, 204, 1000, 50, 1000];
 // Click and drag the mouse to view the scene from different angles.
+
+function preload() {
+  img = loadImage('imgs/debug.jpg');
+}
 
 function setup() {
   createCanvas(1000, 1000, WEBGL);
-
-  describe("A white sphere on a gray background.");
 }
-let r = 2;
 
 function draw() {
+  //planet
+  push();
   background(200);
+  stroke(255, 0, 0);
+  noStroke();
+  rotate(- QUARTER_PI / 3);
+  rotateX(-0.2);
+  rotateY(angle);
+  pointLight(...LightCont);
+  texture(img);
+  angle += 0.002;
+  sphere(100);
+  pop();
 
-  // Draw the sphere.
-  //   rotateWithFrameCount();
-  rotateZ(2 * r);
-  rotateY(2 * r);
-  r *= 2;
-  sphere();
-}
+  //ring
+  push();
+  // Create a 3D ring
+  rotate(- QUARTER_PI / 3);
+  rotateX(HALF_PI - 0.2);
+  fill(255, 255, 0);
+  noStroke();
+  pointLight(...LightCont);
+  torus(150, 10); // (Radius of ring, Radius of tube)
+  pop();
 
-function rotateWithFrameCount() {
-  rotateZ(frameCount);
-  rotateY(frameCount);
 }
